3932ae595e0ae7066a8e9a5b5286c3c0915dc149
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-03-24 13:42:37 +0100

    prepare the project to login & dashboard views

---
+++ resources/frontend/src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dashboard">
+        DASHBOARD
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+    export default {
+        name: "Dashboard",
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/frontend/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Login">
+        LOGIN
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
