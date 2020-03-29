63437cbe505465652d8fdac5eaada928cf7a3603
Julien SEIXAS <32927775+MrZyr0@users.noreply.github.com>
2020-03-29 20:20:56 +0200

    Init vue app for frontend (#14)
    * Update Laravel to v7
    
    commit 4a242fd15775450a1cb3cfc74d1dd146058dd071
    Author: Julien SEIXAS <32927775+MrZyr0@users.noreply.github.com>
    Date:   Mon Mar 9 13:46:43 2020 +0100
    
        Update laravel (add composer.lock) (#11)
    
        * updating laravel version
    
        * Updating forgotten dependencies
    
        Update PHP version (according to documentation : https://laravel.com/docs/7.x/upgrade#upgrade-7.0)
        Update fideloper/proxy & mockery/mockery (according a youtube video: www.youtube.com/watch?v=8gyAkicohsU/#description)
    
        * Replace Handler by the new one
    
        Replaced the Handler.php file by the new file of version 7 in the repo : https://github.com/laravel/laravel/blob/master/app/Exceptions/Handler.php
    
        * update composer.lock
    
        Co-authored-by: Nicolas <larochenicolas.info@gmail.com>
    
    commit 80e06cc7661c8d035094e9f132b6e66c3f4e67db
    Author: Laroche-Nicolas <44775996+Laroche-Nicolas@users.noreply.github.com>
    Date:   Mon Mar 9 13:19:30 2020 +0100
    
        updating laravel version (#10)
    
        * updating laravel version
    
        * Updating forgotten dependencies
    
        Update PHP version (according to documentation : https://laravel.com/docs/7.x/upgrade#upgrade-7.0)
        Update fideloper/proxy & mockery/mockery (according a youtube video: www.youtube.com/watch?v=8gyAkicohsU/#description)
    
        * Replace Handler by the new one
    
        Replaced the Handler.php file by the new file of version 7 in the repo : https://github.com/laravel/laravel/blob/master/app/Exceptions/Handler.php
    
        Co-authored-by: Julien SEIXAS <32927775+MrZyr0@users.noreply.github.com>
    
    * delete laravel web resources
    
    * init vue project + configure Laravel for it
    
    Init a complete and clean vue projet configured for Laravel according to this tutorial : https://github.com/starkovsky/laravel-vue-cli#steps-for-scaffolding-from-scratch
    
    * create miror package.json
    
    this file is a simple link to the package.json file of the vue project in resources/frontend
    
    * update gitignore according to new vue project
    
    * cleaning app vue
    
    * prepare the project to login & dashboard views
    
    * add html template
    
    Add configuration to webpack to have a template.
    With this we have fallback on loading error
    
    * setup vuex (store) & vuex (persist)
    
    * setup basic router
    
    + Classic routes
    + Protect all routes except "/login"
    
    * Create basic views
    
    + Login (set username to state on enter it in input)
    + Dashboard (Display welcome message with username)
    + Logout (clear user object in the state)
    
    * Lint
    
    Co-authored-by: Nicolas Laroche <nicolas.laroche@ynov.com>

---
+++ resources/frontend/vue.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { join } = require("path");
+
+module.exports = {
+    devServer: {
+        proxy: "http://localhost",
+        overlay: {
+            warnings: false,
+            errors: true
+        }
+    },
+
+    // output built static files to Laravel's public dir.
+    outputDir: "../../public/frontend-assets",
+
+    // Set the beginning of the URL to access vue assets
+    publicPath: "/frontend-assets",
+
+    // modify the location of the generated HTML file.
+    indexPath: "../../resources/views/frontend.blade.php",
+
+    lintOnSave: true,
+
+    // TODO: Fix integrity
+    // integrity: true,
+
+    transpileDependencies: ["vuex-persist"],
+
+    // TODO: Fixed webpack configuration to keep the same result as without the configuration
+    configureWebpack: {
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: join(__dirname, "/src/index.html"),
+                title: "SCT Backoffice",
+                description: "SCT Backoffice"
+            })
+        ]
+    }
+};
