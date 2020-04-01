181a190c92960df814af415d823fd0e3bfdba68a
Nicolas Laroche <nicolas.laroche@ynov.com>
2020-04-01 21:44:16 +0200

    migration for Entreprise

--- database/migrations/2020_02_11_153403_create_table_entreprise.php
+++ database/migrations/2020_02_11_153403_create_table_entreprise.php
@@ -23,12 +23,26 @@ class CreateTableEntreprise extends Migration
                 $table->integer('street_number');
                 $table->string('street_name');
                 $table->integer('zipcode');
-                $table->string('town');
-                $table->string('notes',500);
-                $table->string('default_invoice_payment_terms');
+                $table->string('city');
+                $table->integer('capital_in_cents');
+                $table->string('insurance');
+                $table->string('bban',34);
+                $table->boolean('bban_anytmie');
+                $table->smallInteger('default_revive_delay_in_days');
+                $table->smallInteger('default_payment_delay_in_days');
+                $table->string('default_payment_terms');
+                $table->enum('default_payment_method', ['credit_cardbank_transfer','bank_check','cash','multiple']);
+                $table->integer('default_down_payment_percentage');
+                $table->smallInteger('default_quote_validity_delay_in_days');
                 $table->string('default_quote_accepting_conditions');
                 $table->string('default_invoice_notice');
                 $table->string('default_quote_notice');
+                $table->string('notes',500);
+                $table->unsignedBigInteger('customer');
+
+                // mise en place de la contrainte sur la table
+                $table->foreign('customer')->references('in')->on('customers');
+
         });
     }
 
