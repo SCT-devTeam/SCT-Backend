6103a095550b1e0d87f4fa4f4c1afae6827ba76b
Nicolas Laroche <nicolas.laroche@ynov.com>
2020-04-02 15:24:48 +0200

    finish migration invoices_items

--- database/migrations/2020_04_02_123743_create_invoices_items_table.php
+++ database/migrations/2020_04_02_123743_create_invoices_items_table.php
@@ -15,7 +15,18 @@ class CreateInvoicesItemsTable extends Migration
     {
         Schema::create('invoices_items', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer(('invoices_id'));
+            $table->integer(('invoice_id'));
+            $table->enum('item_type',['product','service']);
+            $table->integer('item_id');
+            $table->string('label',100);
+            $table->string('description');
+            $table->integer('pre_vat_price_in_cents');
+            $table->integer('var_rate');
+            $table->integer('quantity');
+
+            //setup constraint
+            $table->foreign('invoice_id')->references('id')->on('invoices');
+            $table->foreign('item_id')->references('id')->on('items');
         });
     }
 
