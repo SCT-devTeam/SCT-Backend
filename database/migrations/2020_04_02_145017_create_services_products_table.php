7c505ae75b6db448c5ee4d0def1cb28b159c1522
Nicolas Laroche <nicolas.laroche@ynov.com>
2020-04-02 16:59:16 +0200

    migration table Services_Products

---
+++ database/migrations/2020_04_02_145017_create_services_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('company_id');
+            $table->string('label',100);
+            $table->string('description');
+            $table->integer('pre_vat_price_in_cents');
+            $table->integer('vat_rate');
+            $table->enum('item_type',['service','product']);
+            $table->integer('quantity');
+
+            //setup constraint
+            $table->foreign('company_id')->references('id')->on('companies');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services_products');
+    }
+}
