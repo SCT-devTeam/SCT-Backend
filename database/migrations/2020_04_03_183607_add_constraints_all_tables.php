a69ce35d696913cf7504f3be2bb551136898bc3f
Nicolas Laroche <nicolas.laroche@ynov.com>
2020-04-03 20:54:05 +0200

    setup all constraints in one migration

--- database/migrations/2020_04_03_183607_add_constraints_all_tables.php
+++ database/migrations/2020_04_03_183607_add_constraints_all_tables.php
@@ -13,7 +13,42 @@ class AddConstraintsAllTables extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('companies')->references('id')->on('companies');
+        });
+
+        Schema::table('customers', function (Blueprint $table) {
+            $table->foreign('company')->references('id')->on('companies');
+        });
+
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->foreign('customer')->references('id')->on('customers');
+        });
+
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->foreign('original_quote')->references('id')->on('quotes');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+
+        Schema::table('invoices_items', function (Blueprint $table) {
+            $table->foreign('invoice_id')->references('id')->on('invoices');
+            $table->foreign('item_id')->references('id')->on('services_products');
+        });
+
+        Schema::table('quotes', function (Blueprint $table) {
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+
+        Schema::table('quotes_items', function (Blueprint $table) {
+            $table->foreign('quote_id')->references('id')->on('quotes');
+            $table->foreign('item_id')->references('id')->on('services_products');
+        });
+
+        Schema::table('services_products', function (Blueprint $table) {
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
     }
 
     /**
