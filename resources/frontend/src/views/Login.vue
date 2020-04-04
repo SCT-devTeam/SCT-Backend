694537ff4d2965f4c9c0a69fb4433de2fad83cdf
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-04 14:45:47 +0200

    update login view to use login form component

--- resources/frontend/src/views/Login.vue
+++ resources/frontend/src/views/Login.vue
@@ -1,39 +1,24 @@
 <template>
     <div id="Login">
-        <h1>Login</h1>
-
-        <form @submit.prevent="updateUser">
-            <input type="text" v-model="username" placeholder="username" />
-        </form>
+        <LoginForm></LoginForm>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import LoginForm from "@/components/LoginForm";
 
-import { mapMutations } from "vuex";
-import store from "../store/index";
-
+/* eslint-disable */
 export default {
     name: "Login",
-    data() {
-        return {
-            username: ""
-        };
-    },
-    methods: {
-        ...mapMutations(["UPDATE_CURRENT_USER"]),
-        updateUser: function() {
-            this.UPDATE_CURRENT_USER({
-                name: this.username
-            });
-            this.username = "";
-            console.log("[STATE] user = " + JSON.stringify(store.state.user));
-            console.log("[STATE] isLoggedIn = " + store.getters.isLoggedIn);
-            this.$router.replace({ name: "Dashboard" });
-        }
-    }
+    components: { LoginForm },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    @import "src/sass/colors";
+    @import "src/sass/typography";
+    body {
+        background-color: var(--colors-main);
+    }
+</style>
