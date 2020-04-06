b83e78f455a5d2b932ab4570a6817cedb17406f2
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-06 23:05:12 +0200

    create input type text

---
+++ resources/frontend/src/components/inputs/TextSCT.vue
@@ -0,0 +1,276 @@
+<template>
+    <div class="InputText" @mouseover="hovered" @mouseleave="notHovered">
+        <label
+            v-bind:for="name"
+            ref="label"
+            v-bind:style="{ top: labelPosition.top, left: labelPosition.left }"
+            @mouseover="hovered"
+            >{{ label }}</label
+        >
+        <span
+            class="outline-top"
+            v-bind:style="{
+                left: topLineOffset + 'px',
+                'background-color': outlineColor
+            }"
+            @mouseover="hovered"
+        ></span>
+        <input
+            type="text"
+            v-bind:id="name"
+            ref="input"
+            v-bind:name="name"
+            v-bind:placeholder="placeholder"
+            v-bind:title="title"
+            :required="isRequired"
+            :disabled="!isEditable"
+            v-model="value"
+            v-bind:class="{ hovered: isHovered, active: isActive }"
+            v-on:keyup="newKeyboardAction"
+            v-on:focusin="active"
+            v-on:focusout="notActive"
+            @mouseover="hovered"
+        />
+        <span
+            class="outline-bot"
+            v-bind:style="{ 'background-color': outlineColor }"
+            @mouseover="hovered"
+        ></span>
+
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 40"
+            id="border-left"
+            @mouseover="hovered"
+        >
+            <path
+                d="M1251.006,687h0a19,19,0,0,0,.016-38"
+                transform="translate(-1251.006 -648)"
+                fill="none"
+                v-bind:stroke="outlineColor"
+                stroke-width="2"
+            />
+        </svg>
+
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 40"
+            id="border-right"
+            @mouseover="hovered"
+        >
+            <path
+                d="M1251.006,687h0a19,19,0,0,0,.016-38"
+                transform="translate(-1251.006 -648)"
+                fill="none"
+                v-bind:stroke="outlineColor"
+                stroke-width="2"
+            />
+        </svg>
+    </div>
+</template>
+
+<script>
+// Change inputs by stylise vuetify inputs : https://vuetifyjs.com/en/components/text-fields/#text-fields
+export default {
+    name: "TextSCT",
+    mounted() {
+        if (this.value.length > 0) {
+            this.toggleTopLine();
+            this.toggleLabel();
+        }
+    },
+    data() {
+        return {
+            value: this.defaultValue,
+            error: "",
+            isHovered: false,
+            isActive: false,
+            topLineOffset: 20,
+            labelPosition: {
+                top: "50%",
+                left: "15px"
+            },
+            outlineColor: "var(--colors-secondary-principal)"
+        };
+    },
+    props: {
+        name: {
+            type: String,
+            default: "text"
+        },
+        title: {
+            type: String,
+            default: "Enter your text here"
+        },
+        placeholder: {
+            type: String,
+            default: "Text"
+        },
+        label: {
+            type: String,
+            default: "Text"
+        },
+        defaultValue: String,
+        isRequired: {
+            type: Boolean,
+            default: false
+            // TODO: fix required markup to the correct HTML5 syntax
+        },
+        isEditable: {
+            type: Boolean,
+            default: true,
+        }
+    },
+    computed: {
+        isFilled() {
+            return !!this.value;
+        }
+    },
+    methods: {
+        hovered() {
+            if (!this.isHovered && !this.isActive && !this.isFilled) {
+                this.isHovered = true;
+                this.toggleTopLine();
+                this.toggleLabel()
+            }
+        },
+        notHovered() {
+            this.isHovered = false;
+            if (!this.isActive && !this.isFilled) {
+                this.toggleTopLine();
+                this.toggleLabel()
+            }
+        },
+        active() {
+            if (!this.isActive) {
+                this.isActive = !this.isActive;
+
+                if (this.isActive) {
+                    this.toggleLabel();
+                    this.outlineColor = "var(--colors-active)";
+                }
+            }
+        },
+        notActive() {
+            this.isActive = false;
+            if (!this.isFilled) {
+                this.toggleTopLine();
+                this.toggleLabel()
+            }
+            this.outlineColor = "var(--colors-secondary-principal)";
+        },
+        toggleTopLine() {
+            if (this.topLineOffset === 20 && (this.isFilled || this.isHovered)) {
+                this.topLineOffset += this.$refs.label.clientWidth + 10;
+            } else {
+                this.topLineOffset = 20;
+            }
+        },
+        toggleLabel() {
+            if (this.labelPosition.top === "0" && !this.isFilled && !this.isHovered) {
+                this.labelPosition.top = "50%";
+                this.labelPosition.left = "15px";
+            } else {
+                this.labelPosition.top = "0";
+                this.labelPosition.left = "25px";
+            }
+        },
+        newKeyboardAction() {
+            this.$emit("valueChanged", this.value);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "src/sass/colors";
+@import "src/sass/typography";
+
+* {
+    transition: all 300ms ease-in-out;
+}
+
+div.InputText {
+    position: relative;
+    border: 5px solid transparent;
+
+    > label {
+        position: absolute;
+        top: 50%;
+        left: 15px;
+        transform: translateY(-50%);
+        z-index: 1;
+
+        cursor: default;
+
+        color: $color__inactive_subtitle;
+        font-family: $font__subtitle;
+    }
+
+    > span {
+        position: absolute;
+        left: 20px;
+        right: 20px;
+        z-index: 2;
+
+        height: 2px;
+
+        background-color: $color__secondary;
+
+        &.outline-top {
+            top: -2px;
+        }
+
+        &.outline-bot {
+            bottom: -2px;
+        }
+    }
+
+    > input {
+        height: 40px;
+        width: calc(100% - 30px);
+
+        padding: 0 15px;
+
+        position: relative;
+
+        border-radius: 100px;
+        border: 0;
+        background-color: #{$color__secondary}4D;
+
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+
+        font-family: $font__text;
+        font-size: 1em;
+        color: $color__black;
+
+        outline: none;
+
+        &::placeholder {
+            color: transparent;
+        }
+    }
+
+    > svg {
+        position: absolute;
+        top: -2px;
+        height: calc(100% + 4px);
+
+        &#border-left {
+            left: -2px;
+            transform: scale(-1, 1);
+        }
+
+        &#border-right {
+            right: -2px;
+        }
+    }
+
+    > p.error {
+        position: absolute;
+        bottom: -20px;
+        left: 5px;
+        margin: 0;
+    }
+}
+</style>
