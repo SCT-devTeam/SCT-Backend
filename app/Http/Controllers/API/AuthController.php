0789dcde5a3e8c5724804fdf767556b1c8758f73
Laroche-Nicolas <44775996+Laroche-Nicolas@users.noreply.github.com>
2020-04-06 16:58:00 +0200

    Features/setup airlock (#18)
    * add airlock to project
    
    * update migration
    
    * adding routes
    
    * add model PersonalAccessTokens
    
    * update user model and controller
    
    * add conf for Sanctum
    
    * adding seeder for User
    
    * adding ide-helpers for Jetbrains PhpStorm
    
    * add API repo
    
    * fix errors for Sanctum
    
    * change bcrypt to Hash
    
    * update architecture for MVC
    
    * upatde login
    
    * end airlock -> Sanctum
    
    * Revert "change bcrypt to Hash"
    
    This reverts commit bb296fed95f348ad27f0f31427137a545e76feb9.
    
    * ROAR

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -24,14 +24,19 @@ class AuthController extends Controller
 
         // 2
         $input = $request->all();
-
-        $usr = new User();
-
-        $usr->getRegister($input);
-
-
+        $input['password'] = Hash::make($input['password']);
+        /** @var User $user */
+        $user = User::firstOrCreate([
+            'email'=>$request->email,
+        ],[
+            'gender'=>$request->gender,
+            'lastname'=>$request->lastname,
+            'firstname'=>$request->firstname,
+            'phone'=>$request->number,
+            'pwd'=> Hash::make($request->pwd),
+        ]);
         // 3
-        $token = $usr->createToken($input->device_name)->plainTextToken;
+        $token = $user->createToken($input->device_name)->plainTextToken;
         // 4
         return response()->json(['token' => $token], 200);
     }
@@ -44,15 +49,10 @@ class AuthController extends Controller
             'device_name' => 'required'
         ]);
         // 1
-/** @var User $user */
-
-        $user = new User();
-
-        $user = $user->getLogin($request);
-
-
+        /** @var User $user */
+        $user = User::where('email', $request->email)->first();
         // 2
-
+//        dd($user);
         if (!$user || !Hash::check($request->password, $user->pwd)) {
             return response()->json(['error' => 'The provided credentials are incorrect.'], 422);
         }
