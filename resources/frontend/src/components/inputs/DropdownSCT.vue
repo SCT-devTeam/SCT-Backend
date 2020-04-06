866e00d4f25068e12765679b784c65c003981e00
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-06 17:33:22 +0200

    create dropdown input

---
+++ resources/frontend/src/components/inputs/DropdownSCT.vue
@@ -0,0 +1,273 @@
+<template>
+    <div class="Input-dropdown" @mouseover="hovered" @mouseleave="notHovered">
+        <label
+            v-bind:for="name"
+            ref="label"
+            v-bind:style="{ top: labelPosition.top, left: labelPosition.left }"
+            @mouseover="hovered"
+        >{{ label }}</label
+        >
+        <span
+            class="outline-top"
+            v-bind:style="{
+                left: topLineOffset + 'px',
+                'background-color': outlineColor
+            }"
+            @mouseover="hovered"
+        ></span>
+
+
+        <select
+            id="dropdown-menu"
+            v-bind:name="name"
+            ref="input"
+            v-bind:title="title"
+            autocomplete="off"
+            :required="isRequired"
+            :disabled="isDisabled"
+            v-model="value"
+            v-bind:class="{ hovered: isHovered, active: isActive }"
+            v-on:keyup="newKeyboardAction"
+            v-on:focusin="active"
+            v-on:focusout="notActive"
+            @mouseover="hovered"
+        >
+            <option v-for="option in options">{{option}}</option>
+        </select>
+
+        <span
+            class="outline-bot"
+            v-bind:style="{ 'background-color': outlineColor }"
+            @mouseover="hovered"
+        ></span>
+
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 40"
+            id="border-left"
+            @mouseover="hovered"
+        >
+            <path
+                d="M1251.006,687h0a19,19,0,0,0,.016-38"
+                transform="translate(-1251.006 -648)"
+                fill="none"
+                v-bind:stroke="outlineColor"
+                stroke-width="2"
+            />
+        </svg>
+
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 40"
+            id="border-right"
+            @mouseover="hovered"
+        >
+            <path
+                d="M1251.006,687h0a19,19,0,0,0,.016-38"
+                transform="translate(-1251.006 -648)"
+                fill="none"
+                v-bind:stroke="outlineColor"
+                stroke-width="2"
+            />
+        </svg>
+    </div>
+</template>
+
+<script>
+    // Change inputs by stylise vuetify inputs : https://vuetifyjs.com/en/components/text-fields/#text-fields
+    export default {
+        name: "DropdownSCT",
+        data() {
+            return {
+                value: "",
+                error: "",
+                isHovered: false,
+                isActive: false,
+                topLineOffset: 20,
+                labelPosition: {
+                    top: "50%",
+                    left: "15px"
+                },
+                outlineColor: "var(--colors-secondary-principal)"
+            };
+        },
+        props: {
+            name: {
+                type: String,
+                default: "dropdown"
+            },
+            title: {
+                type: String,
+                default: "Select an option"
+            },
+            label: {
+                type: String,
+                default: "Dropdown"
+            },
+            options: {
+                type: Array,
+                default: () => ([
+                    'Prospect',
+                    'Active',
+                    'Archived',
+                    'Deleted'
+                ])
+            },
+            isRequired: {
+                type: Boolean,
+                default: false
+                // TODO: fix required markup to the correct HTML5 syntax
+            },
+            isDisabled: {
+                type: Boolean,
+                default: false
+                // TODO: fix required markup to the correct HTML5 syntax
+            }
+        },
+        computed: {
+            isFilled() {
+                return !!this.value;
+            }
+        },
+        methods: {
+            hovered() {
+                if (!this.isHovered && !this.isActive && !this.isFilled) {
+                    this.isHovered = true;
+
+                    this.updateTopLine();
+                    this.labelPosition.top = "0";
+                    this.labelPosition.left = "25px";
+                }
+            },
+            notHovered() {
+                this.isHovered = false;
+                if (!this.isActive && !this.isFilled) {
+                    this.updateTopLine();
+                    this.labelPosition.top = "50%";
+                    this.labelPosition.left = "15px";
+                }
+            },
+            active() {
+                if (!this.isActive) {
+                    this.isActive = !this.isActive;
+
+                    if (this.isActive) {
+                        this.labelPosition.top = "0";
+                        this.labelPosition.left = "25px";
+                        this.outlineColor = "var(--colors-active)";
+                    } else if (!this.isFilled) {
+                        this.labelPosition.top = "50%";
+                        this.labelPosition.left = "15px";
+                    }
+                }
+            },
+            notActive() {
+                this.isActive = false;
+                if (!this.isFilled) {
+                    this.updateTopLine();
+                    this.labelPosition.top = "50%";
+                    this.labelPosition.left = "15px";
+                }
+                this.outlineColor = "var(--colors-secondary-principal)";
+            },
+            updateTopLine() {
+                if (this.topLineOffset === 20) {
+                    this.topLineOffset += this.$refs.label.clientWidth + 10;
+                } else {
+                    this.topLineOffset = 20;
+                }
+            },
+            newKeyboardAction() {
+                this.$emit("valueChanged", this.value);
+            },
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    @import "src/sass/colors";
+    @import "src/sass/typography";
+
+    * {
+        transition: all 300ms ease-in-out;
+    }
+
+    div.Input-dropdown {
+        position: relative;
+        border: 5px solid transparent;
+
+        > label {
+            position: absolute;
+            top: 50%;
+            left: 15px;
+            transform: translateY(-50%);
+            z-index: 1;
+
+            cursor: text;
+
+            color: $color__inactive_subtitle;
+            font-family: $font__subtitle;
+        }
+
+        > span {
+            position: absolute;
+            left: 20px;
+            right: 20px;
+            z-index: 2;
+
+            height: 2px;
+
+            background-color: $color__secondary;
+
+            &.outline-top {
+                top: -2px;
+            }
+
+            &.outline-bot {
+                bottom: -2px;
+            }
+        }
+
+        > select {
+            height: 40px;
+            width: 100%;
+
+            padding: 0 15px;
+
+            position: relative;
+
+            border-radius: 100px;
+            border: 0;
+            background-color: #{$color__secondary}4D;
+
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+
+            outline: none;
+
+            font-family: $font__text;
+            font-size: 1em;
+        }
+
+        > svg {
+            position: absolute;
+            top: -2px;
+            height: calc(100% + 4px);
+
+            &#border-left {
+                left: -2px;
+                transform: scale(-1, 1);
+            }
+
+            &#border-right {
+                right: -2px;
+            }
+        }
+
+        > p.error {
+            position: absolute;
+            bottom: -20px;
+            left: 5px;
+            margin: 0;
+        }
+    }
+</style>
