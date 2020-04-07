61ec1183bda423f5515b5d46e0f23ecd305df9c5
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-07 14:54:12 +0200

    create WritingsTable & Writings view

---
+++ resources/frontend/src/components/WritingsTable.vue
@@ -0,0 +1,135 @@
+<template>
+    <div id="Customers-table">
+        <span id="head">
+            <p class="date">Date</p>
+            <p class="status">Status</p>
+            <p class="customerName">Customer</p>
+            <p class="status">Amount</p>
+        </span>
+        <div id="container">
+            <span class="line" v-for="writing in this.writings">
+                <p class="date">{{writing.date}}</p>
+                <p class="status">{{writing.status}}</p>
+                <p class="customerName">{{writing.customerName}}</p>
+                <p class="amount">{{writing.amount}}</p>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        customerName: "CustomersTable",
+        data() {
+            return {
+                writings: [
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                    {date: '15/03/2020', customerName: 'John Doe', amount: '150,00€', status: 'Paid'},
+                ]
+            }
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "src/sass/colors";
+    @import "src/sass/typography";
+    div#Customers-table {
+        display: flex;
+        flex-direction: column;
+
+        position: relative;
+
+        max-height: calc(100% - 130px);
+
+        padding-top: 60px;
+
+        border-radius: 15px;
+
+        background: #fff;
+
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
+
+        span {
+            display: flex;
+            align-items: center;
+
+            max-width: 100%;
+
+            margin-bottom: 10px;
+            padding: 0 5%;
+
+            background-color: #ececec80;
+
+            > p {
+                flex: 1;
+            }
+        }
+
+        > span#head {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+
+            height: 60px;
+
+            border-radius: 15px 15px 0 0;
+
+            background: $color__main;
+
+            > p {
+                font-family: $font__heading;
+                font-size: 2em;
+            }
+        }
+
+        > div#container {
+            overflow-y: scroll;
+
+            scrollbar-color: $color__inactive_subtitle;
+            scrollbar-width: 10px;
+
+            &::-webkit-scrollbar {
+                width: 10px;
+            }
+
+            &::-webkit-scrollbar-thumb {
+                border-radius: 5px;
+
+                background-color: $color__inactive_subtitle;
+            }
+
+            &::-webkit-scrollbar-track {
+                border-radius: 5px;
+
+                background-color: #{$color__inactive_subtitle}49;
+            }
+
+            > span.line {
+                height: 50px;
+
+                padding-right: calc(5% - 10px);
+
+                > p {
+                    font-family: $font__text;
+                    font-size: 1em;
+                }
+            }
+        }
+    }
+</style>
--- resources/frontend/src/views/Writings.vue
+++ resources/frontend/src/views/Writings.vue
@@ -1,15 +1,30 @@
 <template>
     <div id="writings">
-
+        <SecondaryMenu @navigationChanged="viewDisplayed = $event" :items="['Quotes', 'Invoices']"></SecondaryMenu>
+        <WritingsTable></WritingsTable>
     </div>
 </template>
 
 <script>
+    import SecondaryMenu from "../components/Navigations/SecondaryMenu";
+    import WritingsTable from "../components/WritingsTable";
+
     export default {
-        name: "Writings"
+        name: "Writings",
+        components:{ SecondaryMenu, WritingsTable }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    @import "src/sass/colors";
+    @import "src/sass/typography";
 
+    div#writings {
+        display: flex;
+        flex-direction: column;
+
+        nav:first-child {
+            margin-bottom: 25px;
+        }
+    }
 </style>
