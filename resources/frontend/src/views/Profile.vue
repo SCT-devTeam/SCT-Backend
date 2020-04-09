0d556082771211df87934c56faeac0c7711706c9
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-09 11:11:45 +0200

    create profile view

---
+++ resources/frontend/src/views/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+      <div id="profile">
+          <ProfileCard></ProfileCard>
+          <CompanionCard></CompanionCard>
+      </div>
+</template>
+
+<script>
+    import ProfileCard from "../components/ProfileCard";
+    import CompanionCard from "../components/CompanionCard";
+
+    export default {
+        name: "Profile",
+        components: { ProfileCard, CompanionCard }
+    }
+</script>
+
+<style scoped lang="scss">
+    div#profile {
+        display: flex;
+        justify-content: space-around;
+
+
+    }
+</style>
