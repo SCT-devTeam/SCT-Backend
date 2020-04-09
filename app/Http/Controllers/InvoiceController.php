8ed699539e892a70e0b8a2828fab54d3cb1b10ba
Nicolas Laroche <nicolas.laroche@ynov.com>
2020-04-09 23:10:54 +0200

    update Invoice model and controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -28,52 +28,53 @@ class InvoiceController extends Controller
             'original_quote' => $request->original_quote,
         ]);
 
-        return response()->json(['invoice',$invoice], 200);
+        return response()->json(['invoice', $invoice], 200);
 
     }
-        public function update(Request $request)
+
+    public function update(Request $request)
     {
         $invoice = Invoice::find($request->id);
-
-        $invoice->company_id= $request->company_id;
-        $invoice->customer_id= $request->customer_id;
-        $invoice->qualification= $request->qualification;
-        $invoice->edition_date= $request->edition_date;
+//        dd($invoice);
+        $invoice->company_id = $request->company_id;
+        $invoice->customer_id = $request->customer_id;
+        $invoice->qualification = $request->qualification;
+        $invoice->edition_date = $request->edition_date;
         $invoice->payment_date = $request->payment_date;
-        $invoice->payment_delay_in_days= $request->payment_delay_in_days;
-        $invoice->payment_terms=$request->payment_terms;
-        $invoice->notice= $request->notice;
-        $invoice->payment_method= $request->payment_method;
-        $invoice->sending_date= $request->sending_date;
-        $invoice->revived_date= $request->revived_date;
-        $invoice->last_qualification_date= $request->last_qualification_date;
-        $invoice->chasing_date-> $request=chasing_date;
-        $invoice->note= $request->note;
-        $invoice->original_quote= $request->original_quote;
+        $invoice->payment_delay_in_days = $request->payment_delay_in_days;
+        $invoice->payment_terms = $request->payment_terms;
+        $invoice->notice = $request->notice;
+        $invoice->payment_method = $request->payment_method;
+        $invoice->sending_date = $request->sending_date;
+        $invoice->revived_date = $request->revived_date;
+        $invoice->last_qualification_date = $request->last_qualification_date;
+        $invoice->chasing_date = $request->chasing_date;
+        $invoice->note = $request->note;
+        $invoice->original_quote = $request->original_quote;
 
         $invoice->save();
 
-        return response()->json(['invoice',$invoice], 200);
+        return response()->json(['invoice', $invoice], 200);
     }
 
     public function delete(Request $request)
     {
-        $request->validate(['id'=>'required']);
+        $request->validate(['id' => 'required']);
 
         $invoice = Invoice::find($request->id);
         $invoice->delete();
 
-        return response()->json(['status','Deletion ok'],200);
+        return response()->json(['status', 'Deletion ok'], 200);
     }
 
     public function getInvoice(Request $request)
     {
         $invoices = Invoice::where([
-            ['company_id','=',$request->user()->company],
-            ['customer_id','=',$request->customer_id]
+            ['company_id', '=', $request->user()->company],
+            ['customer_id', '=', $request->customer_id]
         ])->get();
 
-        return response()->json(['invoices'=>$invoices]);
+        return response()->json(['invoices' => $invoices]);
     }
 
 }
