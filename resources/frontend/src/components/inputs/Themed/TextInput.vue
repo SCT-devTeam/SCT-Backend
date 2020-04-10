c646c70ac39540d41a39c6b2b3e7412133e2f3c7
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-10 18:29:58 +0200

    add validation to text input

--- resources/frontend/src/components/inputs/Themed/TextInput.vue
+++ resources/frontend/src/components/inputs/Themed/TextInput.vue
@@ -11,7 +11,7 @@
                 }"
                 @mouseover="hovered"
                 ref="label"
-                >{{ fieldLabel }}</label
+            >{{ fieldLabel }}</label
             >
             <span
                 class="outline-top"
@@ -31,7 +31,7 @@
                     :placeholder="inputPlaceholder"
                     :required="isRequired"
                     :title="title"
-                    v-model:value="value"
+                    :value="value"
                     @focusin="active"
                     @focusout="notActive"
                     @input="onInput"
@@ -81,257 +81,314 @@
 </template>
 
 <script>
-// Change inputs by stylise vuetify inputs : https://vuetifyjs.com/en/components/text-fields/#text-fields
-export default {
-    name: "TextSCT",
-    data() {
-        return {
-            error: "",
-            isHovered: false,
-            isActive: false
-        };
-    },
-    props: {
-        // TODO: implement all attributes
-        name: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        placeholder: String,
-        value: String,
-        type: {
-            type: String,
-            default: "text",
-            validator: (prop) => [
-                'text',
-                'email',
-                'password',
-                'search',
-                'tel',
-            ].includes(prop)
-        },
-        bgColor: {
-            type: String,
-            default: "--colors-secondary-principal"
-        },
-        isRequired: {
-            type: Boolean,
-            default: false
-            // TODO: fix required markup to the correct HTML5 syntax
-        },
-        isDisabled: {
-            type: Boolean,
-            default: false
-            // TODO: fix required markup to the correct HTML5 syntax
-        }
-    },
-    computed: {
-        inputPlaceholder() {
-            if (this.placeholder !== undefined) return this.placeholder;
-            else return this.name;
-        },
-        inputBgColor() {
-            if (this.bgColor.startsWith("--"))
-                return "var(" + this.bgColor + ")";
-            else return this.bgColor;
-        },
-        fieldLabel() {
-            if (this.label !== undefined) return this.label;
-            else return this.name;
-        },
-        isFilled() {
-            return !!this.value;
-        },
-        isInteracting() {
-            return this.isHovered || this.isActive || this.isFilled;
-        },
-        topLineOffset() {
-            if (this.isInteracting) {
-                return (this.$refs.label?.clientWidth || 0) + 30;
-            }
+    // Change inputs by stylise vuetify inputs : https://vuetifyjs.com/en/components/text-fields/#text-fields
+    import {emailValidator} from "../utils/emailValidator"
+    import {passwordValidator} from "../utils/passwordValidator"
 
-            return 20;
-        },
-        labelStyle() {
-            if (this.isInteracting) {
-                return {
-                    position: {
-                        top: "0",
-                        left: "25px"
-                    },
-                    cursor: "default"
-                };
-            }
-
+    export default {
+        name: "TextSCT",
+        data() {
             return {
-                position: {
-                    top: "50%",
-                    left: "15px"
-                },
-                cursor: "text"
+                error: "",
+                isHovered: false,
+                isActive: false,
+                isValid: null,
             };
         },
-        outlineColor() {
-            if (this.isActive) {
-                return "var(--colors-active)";
+        props: {
+            // TODO: implement all attributes
+            name: {
+                type: String,
+                required: true
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            placeholder: String,
+            value: String,
+            type: {
+                type: String,
+                default: "text",
+                validator: (prop) => [
+                    'text',
+                    'email',
+                    'password',
+                    'search',
+                    'tel',
+                ].includes(prop)
+            },
+            bgColor: {
+                type: String,
+                default: "--colors-secondary-principal"
+            },
+            isRequired: {
+                type: Boolean,
+                default: false
+                // TODO: fix required markup to the correct HTML5 syntax
+            },
+            isDisabled: {
+                type: Boolean,
+                default: false
+                // TODO: fix required markup to the correct HTML5 syntax
             }
+        },
+        computed: {
+            inputPlaceholder() {
+                if (this.placeholder !== undefined) return this.placeholder;
+                else return this.name;
+            },
+            inputBgColor() {
+                if (this.bgColor.startsWith("--"))
+                    return "var(" + this.bgColor + ")";
+                else return this.bgColor;
+            },
+            fieldLabel() {
+                if (this.label !== undefined) return this.label;
+                else return this.name;
+            },
+            isFilled() {
+                return !!this.value;
+            },
+            isInteracting() {
+                return this.isHovered || this.isActive || this.isFilled;
+            },
+            topLineOffset() {
+                // TODO: Fix offset on build (label size is null on build...
+                if (this.isInteracting) {
+                    return (this.$refs.label?.clientWidth || 0) + 30;
+                }
 
-            return "var(--colors-secondary-principal)";
+                return 20;
+            },
+            labelStyle() {
+                if (this.isInteracting) {
+                    return {
+                        position: {
+                            top: "0",
+                            left: "25px"
+                        },
+                        cursor: "default"
+                    };
+                }
+
+                return {
+                    position: {
+                        top: "50%",
+                        left: "15px"
+                    },
+                    cursor: "text"
+                };
+            },
+            outlineColor() {
+                if (this.isValid != null && !this.isValid)
+                    return "var(--colors-validation-no)";
+
+                if (this.isValid != null && this.isValid)
+                    return "var(--colors-validation-ok)";
+
+                if (this.isActive)
+                    return "var(--colors-active)";
+
+                return "var(--colors-secondary-principal)";
+            }
+        },
+        methods: {
+            hovered() {
+                this.isHovered = true;
+            },
+            notHovered() {
+                this.isHovered = false;
+            },
+            active() {
+                this.isActive = true;
+                this.isValid = null;
+            },
+            notActive() {
+                this.isActive = false;
+                this.checkInput();
+            },
+            onInput(e) {
+                this.$emit("input", e.target.value);
+                setTimeout(this.checkInput, 700);
+            },
+            checkInput() {
+                // Check if empty
+                if (this.value === "") {
+                    this.error = "Please fill the filed";
+                    this.isValid = false;
+                    this.$emit("isValid", false);
+                } else {
+                    this.error = "";
+                }
+
+                switch (this.type) {
+                    case "email":
+                        if (this.error === "" && !emailValidator(this.value)) {
+                            this.error = "Please enter a correct email";
+                            this.isValid = false;
+
+                            this.$emit("isValid", false);
+                        } else if (
+                            this.error === "" &&
+                            emailValidator(this.value)
+                        ) {
+                            this.isValid = true;
+
+                            this.$emit("isValid", true);
+                        }
+                        break;
+
+                    case "password":
+                        if (this.error === "" && !passwordValidator(this.value)) {
+                            this.error = "Please enter a stronger password (10 characters with 2 of low & UPPER case + special characters + numbers";
+                            this.isValid = false;
+
+                            this.$emit("isValid", false);
+                        } else if (
+                            this.error === "" &&
+                            emailValidator(this.value)
+                        ) {
+                            this.isValid = true;
+
+                            this.$emit("isValid", true);
+                        }
+                        break;
+                }
+            }
         }
-    },
-    methods: {
-        hovered() {
-            this.isHovered = true;
-        },
-        notHovered() {
-            this.isHovered = false;
-        },
-        active() {
-            this.isActive = true;
-        },
-        notActive() {
-            this.isActive = false;
-        },
-        onInput(e) {
-            this.$emit("input", e.target.value);
-        }
-    }
-};
+    };
 </script>
 
 <style lang="scss" scoped>
-@import 'src/scss/colors';
-@import 'src/scss/typography';
+    @import 'src/scss/colors';
+    @import 'src/scss/typography';
 
-* {
-    transition: all 300ms ease-in-out;
-}
+    * {
+        transition: all 300ms ease-in-out;
+    }
 
-div.container {
-    display: flex;
-    flex-direction: column;
-
-    div.filed {
+    div.container {
         display: flex;
-        position: relative;
+        flex-direction: column;
 
-        min-width: 100px;
-        min-height: 40px;
-
-        border: 2px solid transparent;
-        border-top-width: 5px;
-
-        > label {
-            position: absolute;
-            top: 50%;
-            left: 15px;
-            transform: translateY(-50%);
-            z-index: 1;
-
-            color: $color__inactive_subtitle;
-            font-family: $font__subtitle;
-        }
-
-        > span {
-            position: absolute;
-            left: 20px;
-            right: 20px;
-            z-index: 2;
-
-            height: 2px;
-
-            background-color: $color__secondary;
-
-            &.outline-top {
-                top: -2px;
-            }
-
-            &.outline-bot {
-                bottom: -2px;
-            }
-        }
-
-        > div.input-container {
+        div.filed {
             display: flex;
-            justify-content: stretch;
-            align-items: stretch;
-
-            width: 100%;
-
             position: relative;
 
-            border-radius: 20000px;
+            min-width: 100px;
+            min-height: 40px;
 
-            background-color: transparent;
+            border: 2px solid transparent;
+            border-top-width: 5px;
 
-            overflow: hidden;
-            cursor: text;
+            > label {
+                position: absolute;
+                top: 50%;
+                left: 15px;
+                transform: translateY(-50%);
+                z-index: 1;
 
-            > input {
-                min-height: 40px;
-                width: calc(100% - 30px);
+                color: $color__inactive_subtitle;
+                font-family: $font__subtitle;
+            }
 
-                padding: 0 15px;
+            > span {
+                position: absolute;
+                left: 20px;
+                right: 20px;
+                z-index: 2;
 
-                border-radius: 20000px;
-                border: 0;
-                background-color: transparent;
+                height: 2px;
 
-                box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+                background-color: $color__secondary;
 
-                font-family: $font__text;
-                font-size: 1em;
-                color: $color__black;
+                &.outline-top {
+                    top: -2px;
+                }
 
-                outline: none;
-
-                &::placeholder {
-                    color: transparent;
+                &.outline-bot {
+                    bottom: -2px;
                 }
             }
 
-            &:before {
-                content: "";
-                position: absolute;
-                top: 0;
-                bottom: 0;
-                left: 0;
-                right: 0;
-                z-index: -1;
+            > div.input-container {
+                display: flex;
+                justify-content: stretch;
+                align-items: stretch;
+
+                width: 100%;
+
+                position: relative;
 
                 border-radius: 20000px;
 
-                background-color: var(--inputBgColor);
-                opacity: 0.3;
+                background-color: transparent;
+
+                overflow: hidden;
+                cursor: text;
+
+                > input {
+                    min-height: 40px;
+                    width: calc(100% - 30px);
+
+                    padding: 0 15px;
+
+                    border-radius: 20000px;
+                    border: 0;
+                    background-color: transparent;
+
+                    box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+
+                    font-family: $font__text;
+                    font-size: 1em;
+                    color: $color__black;
+
+                    outline: none;
+
+                    &::placeholder {
+                        color: transparent;
+                    }
+                }
+
+                &:before {
+                    content: "";
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    z-index: -1;
+
+                    border-radius: 20000px;
+
+                    background-color: var(--inputBgColor);
+                    opacity: 0.3;
+                }
             }
-        }
 
-        > svg {
-            position: absolute;
-            top: -2px;
-            height: calc(100% + 4px);
+            > svg {
+                position: absolute;
+                top: -2px;
+                height: calc(100% + 4px);
 
-            &.outline-left {
-                left: -2px;
-                transform: scale(-1, 1);
+                &.outline-left {
+                    left: -2px;
+                    transform: scale(-1, 1);
+                }
+
+                &.outline-right {
+                    right: -2px;
+                }
             }
 
-            &.outline-right {
-                right: -2px;
+            > p.error {
+                margin: 0;
+
+                color: $color__validation_no;
+
+                line-break: anywhere;
             }
-        }
-
-        > p.error {
-            margin: 0;
-
-            color: $color__validation_no;
-
-            line-break: anywhere;
         }
     }
-}
 </style>
