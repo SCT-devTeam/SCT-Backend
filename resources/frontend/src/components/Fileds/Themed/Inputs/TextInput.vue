40bf15a5f5a0c687beeac6e93039a5c669bcf471
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-13 15:24:26 +0200

    fix imports of TextInput

--- resources/frontend/src/components/Fileds/Themed/Inputs/TextInput.vue
+++ resources/frontend/src/components/Fileds/Themed/Inputs/TextInput.vue
@@ -164,5 +164,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "src/components/Fileds/Themed/Inputs/mixins/outlineInputMixin";
+@import "./mixins/outlineInputMixin";
 </style>
