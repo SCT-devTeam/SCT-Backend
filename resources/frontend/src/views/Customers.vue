5d7b37944f1f4170c06c42eac6d06a9337ba0a66
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-15 15:16:58 +0200

    simplify Customers view by use the new TableSCT component

--- resources/frontend/src/views/Customers.vue
+++ resources/frontend/src/views/Customers.vue
@@ -5,28 +5,56 @@
             @navigationChanged="viewDisplayed = $event"
         ></SecondaryMenu>
         <div id="customers__view">
-            <ProspectsList
-                v-if="this.viewDisplayed === 'Prospects'"
-            ></ProspectsList>
-            <CustomersList
-                v-if="this.viewDisplayed === 'Customers'"
-            ></CustomersList>
+            <TableSCT
+                class="view__table"
+                :columns="[
+                    { name: 'Type', size: '0.7' },
+                    { name: 'Name', size: '0.7' },
+                    { name: 'Address', size: '1' },
+                    { name: 'Status', size: '0.7' }
+                ]"
+                :data="data"
+                :dataPropertyMapping="{
+                    customer_type: 'Type',
+                    lastname: 'Name',
+                    street_name: 'Address',
+                    status: 'Status'
+                }"
+                @itemClicked="currentCustomerID = $event"
+            ></TableSCT>
+            <CustomerProfileCard
+                class="view__card"
+                v-if="currentCustomerID != null"
+                :customer-id="currentCustomerID"
+            ></CustomerProfileCard>
         </div>
     </div>
 </template>
 
 <script>
 import SecondaryMenu from "../components/Navigations/SecondaryMenu";
-import ProspectsList from "./Composed/ProspectsList";
-import CustomersList from "./Composed/CustomersList";
+import TableSCT from "../components/Tables/TableSCT";
+import CustomerProfileCard from "../components/CustomerProfileCard";
 
 export default {
     name: "Customers",
-    components: { SecondaryMenu, ProspectsList, CustomersList },
+    components: { SecondaryMenu, TableSCT, CustomerProfileCard },
     data() {
         return {
-            viewDisplayed: "Prospects"
+            viewDisplayed: "Prospects",
+            currentCustomerID: null
         };
+    },
+    computed: {
+        data() {
+            if (this.viewDisplayed === "Prospects")
+                return this.$store.getters.getProspects;
+            else if (this.viewDisplayed === "Customers")
+                return this.$store.getters.getCustomers;
+            else
+                console.error("An error has occur ed while trying to set data");
+            return null;
+        }
     }
 };
 </script>
@@ -42,6 +70,10 @@ div#customers {
 
         height: calc(100% - 70px);
         padding-top: 30px;
+
+        > .view__table {
+            margin-right: 50px;
+        }
     }
 }
 </style>
