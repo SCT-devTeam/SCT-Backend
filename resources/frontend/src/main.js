ca0095e2377b919565b5eab0141a3a8efcae3654
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-17 00:00:39 +0200

    re-set the token to axios on store restore
    add self-executed function to vuex to set the token to axios on store restore

--- resources/frontend/src/main.js
+++ resources/frontend/src/main.js
@@ -17,6 +17,16 @@ if (typeof baseURL !== "undefined") {
     Vue.axios.defaults.baseURL = baseURL;
 }
 
+(async function() {
+    console.warn("Restoring the store !");
+    await store.restored;
+    if (store.state.user.token != null) {
+        axios.defaults.headers.common[
+            "Authorization"
+        ] = `Bearer ${store.state.user.token}`;
+    }
+})();
+
 Vue.config.productionTip = false;
 
 // TODO: export the snippet to a separated file
