b4b93bb707be04202e6f7f22e764023563c0b98d
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-16 21:41:02 +0200

    add more TODO

--- resources/frontend/src/views/Logout.vue
+++ resources/frontend/src/views/Logout.vue
@@ -15,6 +15,8 @@ export default {
             set_user: "SET_USER"
         }),
         logoutUser() {
+            // TODO: Extract logout in a vuex action & mutation
+            // add deletation of the default authorization header of axios : delete axios.defaults.headers.Authorization;
             this.set_token(null);
             this.set_user({});
             this.$router.push({ name: "Login" });
