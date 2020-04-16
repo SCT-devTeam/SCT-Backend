3d87a765ec49d0aad8b0ff26f98afeb7c01e0993
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2020-04-16 09:49:13 +0200

    add a todo

--- resources/frontend/src/main.js
+++ resources/frontend/src/main.js
@@ -7,6 +7,7 @@ import VueAxios from "vue-axios";
 
 Vue.use(VueAxios, axios);
 
+// TODO: add env var for baseURL
 const baseURL =
     window.location.hostname === "localhost"
         ? "http://localhost:8000"
